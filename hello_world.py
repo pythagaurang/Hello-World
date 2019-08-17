bc72131707021541e6276392299c9a80ccd8b399
moriarty128 <gaurangsg.28@gmail.com>
2019-08-17 14:16:46 +0530

    python2_to_python3

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,15 @@
 import time
+import os
 array=["H","e","l","l","o"," ","W","o","r","l","d"]
 newarray=[]
 for i in range(len(array)):
     newarray.insert(i," ")
     while newarray[i]!=array[i] :
+        os.system('clear')
         newarray[i]=chr(ord(newarray[i]) + 1)
-
         for j in range(i+1):
-            print newarray[j],
+            print (newarray[j],end='')
             j=j+1
-        
-        time.sleep(0.015)
-        print("\n\n\n")              
+        print()
+        time.sleep(0.015)              
     i+=1
